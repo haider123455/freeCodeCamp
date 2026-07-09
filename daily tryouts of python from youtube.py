b3523d6e0575796858c60134b9ec3870797951f3
onlyyelisha <mukisaelisha10@gmail.com>
2026-07-09 09:12:14 +0300

    day one

---
+++ daily tryouts of python from youtube.py
@@ -0,0 +1,8 @@
+# reversing the order of the words in a list
+given_list=["jolly","elisha","kimberly"]
+#applying the list comprehesion skill
+result_list=[i[::-1] for i in given_list]
+#the double :: ensures the reversing of the words
+#taken to thorny for testing
+print(result_list)#the result is as expected
+#day 1 of practice
