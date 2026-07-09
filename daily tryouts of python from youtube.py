91b315a782d02301c139dae778a64dddb13ecc2d
onlyyelisha <mukisaelisha10@gmail.com>
2026-07-09 14:05:17 +0300

    day one

--- daily tryouts of python from youtube.py
+++ daily tryouts of python from youtube.py
@@ -1,15 +1,15 @@
 # reversing the order of the words in a list
-given_list=["jolly","elisha","kimberly"]
-#applying the list comprehesion skill
-result_list=[i[::-1] for i in given_list]
-#the double :: ensures the reversing of the words
-#taken to thorny for testing
-print(result_list)#the result is as expected
-#day 1 of practice
+given_list = ["jolly", "elisha", "kimberly"]
+# applying the list comprehesion skill
+result_list = [i[::-1] for i in given_list]
+# the double :: ensures the reversing of the words
+# taken to thorny for testing
+print(result_list)  # the result is as expected
+# day 1 of practice
 # day one addons
-#using break points
-mimi=6
-zack=20
-breakpoint()
-total_age=mimi+zack
+# using break points
+mimi = 6
+zack = 20
+breakpoint()  # applied the ide way of putting a breakpoint
+total_age = mimi + zack
 print(total_age)
