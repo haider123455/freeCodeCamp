6049df0880f436d2601f99ac464d4eae912fc4e8
onlyyelisha <mukisaelisha10@gmail.com>
2026-07-10 14:49:17 +0300

    day one

--- daily tryouts of python from youtube.py
+++ daily tryouts of python from youtube.py
@@ -1,15 +1,28 @@
 # reversing the order of the words in a list
-given_list = ["jolly", "elisha", "kimberly"]
+# given_list = ["jolly", "elisha", "kimberly"]
 # applying the list comprehesion skill
-result_list = [i[::-1] for i in given_list]
+# result_list = [i[::-1] for i in given_list]
 # the double :: ensures the reversing of the words
 # taken to thorny for testing
-print(result_list)  # the result is as expected
+# print(result_list)  # the result is as expected
 # day 1 of practice
 # day one addons
 # using break points
-mimi = 6
-zack = 20
-breakpoint()  # applied the ide way of putting a breakpoint
-total_age = mimi + zack
-print(total_age)
+# mimi = 6
+# zack = 20
+# breakpoint()  # applied the ide way of putting a breakpoint
+# total_age = mimi + zack
+# print(total_age)
+# day two making a swapped list
+# swapping the first three items with the last of three
+lis_of_numbers = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+]
+half_size = len(lis_of_numbers) // 2
+swapped_list = lis_of_numbers[half_size:] + lis_of_numbers[:half_size]
+print(swapped_list)
